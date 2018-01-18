41a63a9033333638fc79f7fb439ba6bb44919a81
Sampsa Laapotti <sampsa@laapotti.net>
2018-01-18 02:26:34 +0200

    Hello clojure!

--- dominion-module/src/dominion_module/core.clj
+++ dominion-module/src/dominion_module/core.clj
@@ -1,6 +1,7 @@
-(ns dominion-module.core)
+(ns dominion-module.core
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
